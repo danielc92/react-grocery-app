66203d1e66978f7251192317ea76b98fb1fef18c
Daniel Corcoran <danielc.92@outlook.com>
2019-06-05 15:45:17 +1000

    Changed Grocery Detail

--- src/GroceryDetail.js
+++ src/GroceryDetail.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class GroceryDetail extends Component {
     render() {
         return (
-            <React.Fragment>
-                <article style={{border:'2px solid black', padding: '0.5rem', margin: '1rem'}}>
-                    <h3>{this.props.item.item_name}</h3>
-                    <p>brand: {this.props.item.brand_name}</p>
-                    <p>ingredients: {this.props.item.nf_ingredient_statement}</p>
-                    <code>last updated: {this.props.item.updated_at}</code>
-                </article>
-            </React.Fragment>
+<React.Fragment>
+    <article className="column is-one-third-fullhd is-one-third-desktop is-one-half">
+        <div className="notification">
+            <div className="content">
+                <h3 className="title">{this.props.item.item_name}</h3>
+                <p>{this.props.item.brand_name}</p>
+                <p>{this.props.item.nf_ingredient_statement}</p>
+                <p style={{fontFamily:'monospace'}}>{this.props.item.updated_at}</p>
+            </div>
+        </div>
+    </article>
+</React.Fragment>
         )
     }
 }
--- src/GroceryList.js
+++ src/GroceryList.js
@@ -5,12 +5,18 @@ export default class GroceryList extends Component {
     render() {
         return (
             <React.Fragment>
+            <section className="section">
+            <div className="columns is-multiline">
             {
+                
                 this.props.data.map(item => {
                     return <GroceryDetail key={item.item_id} item={item}
                     />
                 })
+               
             }
+            </div>
+            </section>
             </React.Fragment>
         )
     }
