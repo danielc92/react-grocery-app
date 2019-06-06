0b2c8f283b76a44aa888af1648fdfbb9cb9578f8
Daniel Corcoran <danielc.92@outlook.com>
2019-06-06 15:58:05 +1000

    Formatting changes

--- src/GroceryDetail.js
+++ src/GroceryDetail.js
@@ -24,17 +24,17 @@ export default class GroceryDetail extends Component {
             <div className="content">
                 <img src={'./images/' + this.props.item.image_name}></img>
                 <h5 className="title">{this.props.item.name}</h5>
-                <p className="tag">
+                <p className="tags">
                 <span className="tag is-small is-warning">{this.props.item.category}</span>    
-                <span className="tag is-small is-warning">Stock:&nbsp;   <strong> {this.props.item.stock}</strong></span>    
+                <span className="tag is-small is-success">Stock:&nbsp;   <strong> {this.props.item.stock}</strong></span>    
                 </p>
-                <p className="has-text-info help">{this.props.item.description}</p>
-                <p className="has-text-grey help">{this.props.item.last_updated}</p>
-                <p className="has-text-grey help">{this.props.item.uuid}</p>
+                <p className="help">{this.props.item.description}</p>
+                <p><code style={{ backgroundColor:'transparent', fontSize: '0.8rem', color: '#4873b0' }}>{this.props.item.last_updated}</code></p>
+                <p><code style={{ backgroundColor:'transparent', fontSize: '0.8rem', color: '#4873b0' }}>{this.props.item.uuid}</code></p>
                 <p className="buttons">
-                    <button onClick={this.props.deleteGrocery.bind(this, this.props.item.uuid)} className="button is-outlined is-danger is-small">Delete</button>
-                    <button onClick={this.props.modifyStock.bind(this, this.props.item.uuid, -1)} className="button is-outlined is-warning is-small">Decrement Stock</button>
-                    <button onClick={this.props.modifyStock.bind(this, this.props.item.uuid, 1)} className="button is-outlined is-success is-small">Increment Stock</button>
+                    <button onClick={this.props.deleteGrocery.bind(this, this.props.item.uuid)} className="button is-outlined is-danger is-small">Delete Grocery</button>
+                    <button onClick={this.props.modifyStock.bind(this, this.props.item.uuid, -1)} className="button is-outlined is-success is-small">Purchase One Unit</button>
+                    <button onClick={this.props.modifyStock.bind(this, this.props.item.uuid, 1)} className="button is-outlined is-success is-small">Restock One Unit</button>
                 </p>
             </div>
         </div>
