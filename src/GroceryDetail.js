40ef7241c404d7c143d017208bf0544eda6b0708
Daniel Corcoran <danielc.92@outlook.com>
2019-06-06 15:45:28 +1000

    Removed check exists function in grocerydetail

--- src/GroceryDetail.js
+++ src/GroceryDetail.js
@@ -2,14 +2,6 @@ import React, { Component } from 'react';
 
 export default class GroceryDetail extends Component {
 
-    checkItemExists = (item, item_desc) => {
-        if (item) {
-            return item
-        } else {
-            return `${item_desc} data does not exist for this grocery...`
-        }
-    }
-
     checkStyle = (item) => {
         if (item) {
             return {
@@ -34,11 +26,11 @@ export default class GroceryDetail extends Component {
                 <h5 className="title">{this.props.item.name}</h5>
                 <p className="tags">
                 <span className="tag is-small is-warning">{this.props.item.category}</span>    
-                <span className="tag is-small is-warning">Stock: <strong>{this.props.item.stock}</strong></span>    
+                <span className="tag is-small is-warning">Stock:&nbsp;   <strong> {this.props.item.stock}</strong></span>    
                 </p>
-                <p style={this.checkStyle(this.props.item.description)}>{this.checkItemExists(this.props.item.description, 'Description')}</p>
-                <p className="has-text-grey help">Last updated: {this.props.item.last_updated}</p>
-                <p className="help has-text-link">uuid: {this.props.item.uuid}</p>
+                <p className="has-text-info help">{this.props.item.description}</p>
+                <p className="has-text-grey help">updated: {this.props.item.last_updated}</p>
+                <p className="has-text-grey help">uuid: {this.props.item.uuid}</p>
                 <p className="buttons">
                     <button onClick={this.props.deleteGrocery.bind(this, this.props.item.uuid)} className="button is-outlined is-danger is-small">Delete</button>
                     <button onClick={this.props.modifyStock.bind(this, this.props.item.uuid, -1)} className="button is-outlined is-warning is-small">Decrement Stock</button>
